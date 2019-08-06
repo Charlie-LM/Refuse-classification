4591607348d3b2ca7d7a633f36d717b1372dd866
Charlie-LM <1426726364@qq.com>
2019-08-06 23:11:09 +0800

    8-6

--- Refuse_classification/apps/article/forms.py
+++ Refuse_classification/apps/article/forms.py
@@ -1,2 +1,8 @@
 from django import forms
 from . import models
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label="密码",max_length=18, widget=forms.PasswordInput)
+    password2 = forms.CharField(label="确认密码",max_length=18, widget=forms.PasswordInput)
+
