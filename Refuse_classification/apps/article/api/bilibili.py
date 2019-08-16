372af33e34546a94fa2dfc0b6abc70565a80e729
pzl1208 <563601090@qq.com>
2019-08-16 22:13:07 +0800

    视频入库

--- Refuse_classification/apps/article/api/bilibili.py
+++ Refuse_classification/apps/article/api/bilibili.py
@@ -4,8 +4,7 @@ import pprint
 import time
 import random
 import re
-import os,django
-
+import os, django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Refuse_classification.settings")  # project_name 项目名称
 django.setup()
@@ -15,9 +14,8 @@ header = {
     'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'
 }
-list0 = []
-
-for page in range(1,20):
+n = 1
+for page in range(1, 2):
     url = 'https://api.bilibili.com/x/web-interface/search/type?jsonp=jsonp&&search_type=video&highli' \
           'ght=1&keyword=垃圾分类搞笑视频&page=' + str(
         page)
@@ -25,19 +23,22 @@ for page in range(1,20):
     html = res.content.decode('raw_unicode_escape')
     html_new = json.loads(html, encoding='utf-8')
 
-
     for i in range(len(html_new['data']['result'])):
         aid = html_new['data']['result'][i]['aid']
-        pic = html_new['data']['result'][i]['pic']
-        time_time = html_new['data']['result'][i]['duration']
-        title0 = html_new['data']['result'][i]['title']
-        title = re.sub(r'<em class=\"keyword\">|</em>',"",title0)
-
-        list0.append({'aid': aid, 'pic': pic, 'title': title,'time_time':time_time})
-
-        Interest.objects.create(aid=str['aid'],pic=str['pic'],time_time=str['time_time'],title=str['title'])
-        time.sleep(random.randint(4, 6))
-pprint.pprint(list0)
+        compare = Interest.objects.get(vd_id=aid)
+        if compare:
+            print(f'重复数据{n}')
+            n += 1
+            continue
+        else:
+            pic = html_new['data']['result'][i]['pic']
+            time_time = html_new['data']['result'][i]['duration']
+            title0 = html_new['data']['result'][i]['title']
+            title = re.sub(r'<em class=\"keyword\">|</em>', "", title0)
+            Interest.objects.create(vd_id=aid, avator=pic, time=time_time, title=title)
+            print(f'成功写入第{n}条数据')
+            n += 1
+    time.sleep(random.randint(6, 8))
 # pprint.pprint(aid)
 # pprint.pprint(arcurl)
 # pprint.pprint(pic)
