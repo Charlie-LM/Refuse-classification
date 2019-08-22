43950865ffe02e85911ad39b18d92ae0f828752e
Charlie-LM <1426726364@qq.com>
2019-08-22 23:13:32 +0800

    ojbk

--- Refuse_classification/apps/accounts/views.py
+++ Refuse_classification/apps/accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib import auth
 from django.http import JsonResponse
 import logging
 from .forms import RegisterForm, LoginForm
-from .models import User
+from .models import User,FindPassword
 logger = logging.getLogger("account")
 
 # Create your views here.
@@ -111,3 +111,59 @@ class Login(View):
 def logout(request):
     auth.logout(request)
     return redirect(reverse("article:index"))
+
+import random
+import string
+from django.core.mail import send_mail
+
+class PasswordForget(View):
+    def post(self, request):
+        email = request.POST.get("email")
+        if email and User.objects.filter(email=email):
+            verify_code = "".join(random.choices(string.ascii_lowercase + string.digits, k=128))
+            url = f"{request.scheme}://{request.META['HTTP_HOST']}/accounts/password/reset/{verify_code}?email={email}"
+            ret = FindPassword.objects.get_or_create(email=email)
+            # (<FindPassword: FindPassword object>, True)
+            ret[0].verify_code = verify_code
+            ret[0].status = False
+            ret[0].save()
+            send_mail('注册用户验证信息', url, None, [email])
+            msg = {'code': 200, 'msg': "邮件发送成功，请登录邮箱查看！"}
+            return JsonResponse(msg)
+        else:
+            msg = {'code': 404, 'msg': "输入的邮箱不存在！"}
+            return JsonResponse(msg)
+
+class PasswordReset(View):
+    def get(self, request, verify_code):
+        import datetime
+        create_time_newer = datetime.datetime.utcnow()-datetime.timedelta(minutes=30)
+        email = request.GET.get("email")
+        # 邮箱、verify_code、status=False、时间近30分钟
+        find_password = FindPassword.objects.filter(status=False, verify_code=verify_code, email=email, creat_time__gte=create_time_newer)
+        # great_then_equal, lte, lt, gt
+        if verify_code and find_password:
+            return render(request, "password_reset.html")
+        else:
+            return HttpResponse("链接失效或有误")
+
+    def post(self, request, verify_code):
+        import datetime
+        create_time_newer = datetime.datetime.utcnow() - datetime.timedelta(minutes=30)
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        if password2 == password1:
+            try:
+                find_password = FindPassword.objects.get(status=False, verify_code=verify_code, creat_time__gte=create_time_newer)
+                user = User.objects.get(email=find_password.email)
+                user.set_password(password1)
+                user.save()
+                msg = "重置密码成功，请登录"
+                find_password.status = True
+                find_password.save()
+            except Exception as ex:
+                # 记日志 ex
+                msg = "出错啦"
+        else:
+            msg = "两次密码不一致"
+        return render(request, "password_reset.html", {"msg":msg})
