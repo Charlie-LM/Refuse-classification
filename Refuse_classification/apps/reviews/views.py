3ed65bed53eb722ec40cd74686e16f8c2eda3dd2
pzl1208 <563601090@qq.com>
2019-08-22 17:23:07 +0800

    评论功能完成

--- Refuse_classification/apps/reviews/views.py
+++ Refuse_classification/apps/reviews/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Says, Comments
 from apps.accounts.models import User
 # Create your views here.
@@ -6,50 +7,70 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 
 
-class Reviews(LoginRequiredMixin,View):
+class Reviews(LoginRequiredMixin, View):
     def post(self, request):
         result = {}
         if request.is_ajax:
-            # print(request.user.username)
-            # print(request.POST.get('osize'))
-            # print(request.POST.get('now'))
+            username = request.user.username #发表的人名
+            print("发表的人是： ", username)
+            if request.POST.get('say_id'):
+                print('获取到了say_id:', request.POST.get('say_id'))
+                content = request.POST.get('ohval1') #发表的内容
+                create_time = request.POST.get('now') #发表的时间
 
-            content = request.POST.get('osize')
-            username = request.user.username
-            pic = request.POST.get('pic',"")
-            create_time = request.POST.get('now')
+                user = User.objects.get(username=username) #发表的人的对象
+                beusername = request.POST.get('oHfName').strip() #被评论的人
+                beuser = User.objects.get(username=beusername) #被评论的人的对象
 
-            result = {'content': content, 'username': username, 'create_time': create_time}
+                #被评论的说说对象
+                say_id = Says.objects.get(id=request.POST.get('say_id'))
+                print(say_id)
+                try:
+                    Comments.objects.create(comments=content, says_comments=say_id, beuser_id=beuser,
+                                            user_id=user)
+                    result = {'status': True, 'content': content, 'create_time': create_time, 'username': username,
+                              'beuser': beusername}
+                except:
+                    result = {'status': False, 'content': content, 'create_time': create_time, 'username': username,
+                              'beuser': beusername}
 
+            else:
+                content = request.POST.get('osize')
+                pic = request.POST.get('pic', "")
+                create_time = request.POST.get('now')
 
-            user_id = User.objects.get(username=username)
-            print(user_id)
-            Says.objects.create(user_id=user_id,content=content,pic=pic)
-        return render(request,'reviews.html',result)
+                # print(result)
+                user_id = User.objects.get(username=username)
+                # print(user_id)
+                try:
+                    Says.objects.create(user_id=user_id, content=content, pic=pic)
+                    result = {'status': True, 'content': content, 'username': username, 'create_time': create_time}
+                except:
+                    result = {'status': False, 'content': content, 'username': username, 'create_time': create_time}
+        return JsonResponse(result)
 
-
-
-    def get(self,request):
+    def get(self, request):
         result = []
         says = Says.objects.all().order_by('-creat_time')
-        print(says)
+        # print(says)
         for say in says:
-            temp_dict = {}
-            temp_dict['create_time'] = say.creat_time
-            temp_dict['content'] = say.content
-            temp_dict['username'] = say.user_id.username
-            temp_comments_list = []
+            say_dict = {}
+            say_dict['say_id'] = say.id
+            say_dict['create_time'] = say.creat_time
+            say_dict['content'] = say.content
+            say_dict['username'] = say.user_id.username
+            say_comments_list = []
             comments = Comments.objects.filter(says_comments_id=say.id).all().order_by('-creat_time')
             if comments:
                 for comment in comments:
-                    temp_comment_dict = {}
-                    temp_comment_dict['content'] = comment.comments
-                    temp_comment_dict['create_time'] = comment.creat_time
-                    temp_comment_dict['username'] = comment.user_id.username
-                    temp_comment_dict['beusername'] = comment.beuser_id.username
-                    temp_comments_list.append(temp_comment_dict)
-                temp_dict['comments'] = temp_comments_list
+                    say_comment_dict = {}
+                    say_comment_dict['content'] = comment.comments
+                    say_comment_dict['create_time'] = comment.creat_time
+                    say_comment_dict['username'] = comment.user_id.username
+                    say_comment_dict['beusername'] = comment.beuser_id.username
+                    say_comments_list.append(say_comment_dict)
+                say_dict['comments'] = say_comments_list
             else:
-                temp_dict['comments'] = ""
-            result.append(temp_dict)
+                say_dict['comments'] = []
+            result.append(say_dict)
         return render(request, 'reviews.html', {'result': result})
